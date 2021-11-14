8a9bf4a473769bcd0236205b993f67da9cc05ba6
Kent Huang <lkk223303@gmail.com>
2021-11-14 15:17:20 +0800

    dotenv add src/indexjs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-/* eslint-disable no-console */
 const logger = require("./logger");
+const dotenv = require("dotenv");
 const app = require("./app");
+
+dotenv.config();
 const port = process.env.PORT || app.get("port");
+
 const server = app.listen(port);
-//
+
 process.on("unhandledRejection", (reason, p) =>
   logger.error("Unhandled Rejection at: Promise ", p, reason)
 );
